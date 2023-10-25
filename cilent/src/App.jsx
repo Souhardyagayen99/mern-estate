666368c6a7b6f8f60c8d53a52b7247ab0752ee8e
Souhardya Gayen <64001518+Souhardyagayen99@users.noreply.github.com>
2023-10-26 01:18:04 +0530

    message here

---
+++ cilent/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './assets/pages/Home'
+import SignIn from './assets/pages/SignIn'
+import SignUp from './assets/pages/SignUp'
+import About from './assets/pages/About'
+import Profile from './assets/pages/Profile'
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/"element={<Home/>} />
+        <Route path="/sign-in"element={<SignIn/>} />
+        <Route path="/sign-up"element={<SignUp/>} />
+        <Route path="/about"element={<About/>} />
+        <Route path="/profile"element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
